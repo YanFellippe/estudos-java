9a5d4ce5c449ddb300a5fec0c3f34fd977872c64
Yan Basílio <69776644+YanFellippe@users.noreply.github.com>
2024-08-05 11:23:57 -0300

    Add files via upload

---
+++ Mostrar.java
@@ -0,0 +1,11 @@
+import Aula.Aluno;
+import Aula.Professor;
+
+public class Mostrar {
+    public static void main(String[] args) {
+        Aluno aluno = new Aluno("Yan", 22, "Técnico em Desenvolvimento de Sistemas");
+        aluno.apresentar();
+        Professor professor = new Professor("Rafael Bomfin", 45, "ADS");
+        professor.apresentar();
+    }
+}
